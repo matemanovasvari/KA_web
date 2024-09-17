efc9ebad20d781865376b2f622c9c49c59f1a522
Nagy Máté2 <b1nagmat2@vasvari.org>
2024-09-17 14:16:32 +0200

    htdthsdrg
    sefs

--- javascript_ismétlés/1/index.js
+++ javascript_ismétlés/1/index.js
@@ -1,7 +1,11 @@
-var lenght = document.getElementById("lenght").value;
-var width = document.getElementById("width").value;
-var height = document.getElementById("height").value;
+function Start(){
+    var lenght = parseFloat(document.getElementById("lenght").value);
+    var width = parseFloat(document.getElementById("width").value);
+    var height = parseFloat(document.getElementById("height").value);
 
-var surfaceArea = 2 * (lenght * width + width * height + lenght * height);
+    var surfaceArea = 2 * (lenght * width + width * height + lenght * height);
+    var volume = lenght * width * height;
 
-document.getElementById("SURFICE_AREA").innerHTML = 'The'
+    document.getElementById("SURFICE_AREA").innerHTML = `The surfice area is: ${surfaceArea}`;
+    document.getElementById("VOLUME").innerHTML = `The volume is: ${volume}`;
+}
