f24922ecd760e0f0a2bf0c6d930e92bbbcbc5e91
Nagy Máté2 <b1nagmat2@vasvari.org>
2024-09-17 10:39:32 +0200

    srgr
    gs

---
+++ javascript_ismétlés/1/index.js
@@ -0,0 +1,7 @@
+var lenght = document.getElementById("lenght").value;
+var width = document.getElementById("width").value;
+var height = document.getElementById("height").value;
+
+var surfaceArea = 2 * (lenght * width + width * height + lenght * height);
+
+document.getElementById("SURFICE_AREA").innerHTML = 'The'
