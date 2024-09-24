8c21de6b9727c141152eabfd507e025764cb7d30
Nagy Máté2 <b1nagmat2@vasvari.org>
2024-09-24 15:07:44 +0200

    string_methods

---
+++ String függvények/string.js
@@ -0,0 +1,40 @@
+let text = "A kedvenc számom 3";
+
+/*
+console.log(text.length);
+console.log(text.charAt(2));
+console.log(text.at(-3));
+
+//slice, substring, substr
+console.log(text.slice(2, 9));
+console.log(text.substring(2, 10));
+
+console.log(text.substring(10));
+console.log(text.slice(-8));
+
+console.log(text.substr(2, 8));
+
+//upper
+console.log(text.toUpperCase());
+console.log(text.slice(-8).toUpperCase());
+
+console.log(text.concat(" valami"));
+*/
+
+var text2 = " ihatejavascript ";
+
+//trim
+console.log(text2);
+console.log(">" + text2.trim() + "<");
+console.log(">" + text2.trimEnd() + "<");
+console.log(">" + text2.trimStart() + "<");
+
+
+
+console.log("i wanna live".repeat(10));
+console.log(text.replace("3", "5"));
+console.log(text.replaceAll("e", "ú"));
+
+console.log(text2.split(" "))
+console.log(text2.split(""))
+console.log(text2.split())
