e3c8bad6cbaf6ad7490d4f4711d4aa8704380a04
Nagy Máté2 <b1nagmat2@vasvari.org>
2024-09-24 14:44:24 +0200

    feladatok done

--- javascript_ismétlés/for_loop_04/for_loop_04.js
+++ javascript_ismétlés/for_loop_04/for_loop_04.js
@@ -1,5 +1,7 @@
-let my_string = 'apple';
-
-for (let i = 0; i < my_string.length; i++) {
-    document.write(my_string[i] + "<br>");
+function SplitString(){
+    var my_string = document.getElementById("str").value;
+    
+    for (var i = 0; i < my_string.length; i++) {
+        document.write(my_string[i] + "<br>");
+    }
 }
