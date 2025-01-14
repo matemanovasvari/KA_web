22c0edde2d8609153e7c304fa6ec30d17fdbe635
Nagy Máté2 <b1nagmat2@vasvari.org>
2025-01-14 15:15:26 +0100

    2025. 01. 14. -

---
+++ courses_project/script.js
@@ -0,0 +1,4 @@
+function coursesPage(){
+    document.getElementById("mainPage").style.visibility="hidden";
+    document.getElementById("viewCoursesPage").style.visibility="visible"; 
+}
--- fetch_methods/script.js
+++ fetch_methods/script.js
@@ -1,14 +1,20 @@
 function getMethod(){
   fetch(`https://jsonplaceholder.typicode.com/todos`, {
     method: "GET"
-  })
-  .then(response => response.json())
-  .then(json => {
-    json.forEach(user => {
-      writeToWebpage(user);
+    })
+    .then(response => response.json())
+    .then(json => {
+      let li = `<tr><th>Name</th><th>Email</th></tr>`;
+      json.forEach(user => {
+        li += `<tr>
+        <td>${user.id} </td>
+        <td>${user.userId}</td>
+        <td>${user.title}</td>
+        <td>${user.completed}</td>
+        </tr>`;
+      });
+      document.getElementById("users").innerHTML = li;
     });
-    document.getElementById("users").innerHTML = li;
-  });
 }
 
 function postMethod(titleValue, completedStatus){
@@ -72,14 +78,4 @@ function deleteMethod(id){
   })
   .then(json => console.log(json))
   .catch(error => console.error('There was a problem with the fetch operation:', error));
-}
-
-function writeToWebpage(datas){
-  document.getElementById("id").textContent = datas.id;
-  document.getElementById("userId").textContent = [datas.firstname, datas.lastname].join(" ");
-  document.getElementById("telefon").textContent = datas.phone;
-  document.getElementById("email").textContent = datas.email;
-  document.getElementById("cim").textContent = datas.address.zipcode + " " + datas.address.city + " " + datas.address.street + " " + datas.address.suite;
-  document.getElementById("geo").textContent = datas.address.geo.lat + "; " + datas.address.geo.lng;
-  document.getElementById("cegadat").textContent = datas.company.name + " " + datas.company.catchPhrase + " " + datas.company.bs
 }
